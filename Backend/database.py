3907e34ba278aadf95e3f2a79437e8c2e87ac0bc
Khatisani <khatisanimongwe@gmail.com>
2026-03-22 13:55:33 +0200

    Added more care centres and police stations

--- Backend/database.py
+++ Backend/database.py
@@ -1,16 +1,25 @@
 THUTHUZELA_CENTRES = [
     # GAUTENG
+    {"name": "Charlotte Maxeke Hospital TCC (Joburg Central)", "lat": -26.1755, "lng": 28.0435},
+    {"name": "Leratong Hospital TCC (Krugersdorp)", "lat": -26.1558, "lng": 27.7854},
+    {"name": "Kopanong Hospital TCC (Vereeniging)", "lat": -26.6575, "lng": 27.9421},
     {"name": "Sunnyside TCC (Pretoria)", "lat": -25.7511, "lng": 28.2136},
     {"name": "Chris Hani Baragwanath TCC (Soweto)", "lat": -26.2625, "lng": 27.9439},
     {"name": "Thembisa TCC", "lat": -25.9964, "lng": 28.2272},
     {"name": "Heidelberg TCC", "lat": -26.5047, "lng": 28.3619},
     {"name": "Germiston TCC", "lat": -26.2167, "lng": 28.1667},
     # WESTERN CAPE
+    {"name": "Heideveld Emergency Centre TCC", "lat": -33.9634, "lng": 18.5521},
+    {"name": "Hermanus Hospital TCC", "lat": -34.4167, "lng": 19.2333},
+    {"name": "Worcester Hospital TCC", "lat": -33.6464, "lng": 19.4458},
     {"name": "Karl Bremer TCC (Bellville)", "lat": -33.9031, "lng": 18.6211},
     {"name": "Victoria Hospital TCC (Wynberg)", "lat": -34.0221, "lng": 18.4619},
     {"name": "George TCC", "lat": -33.9587, "lng": 22.4597},
     {"name": "Paarl TCC", "lat": -33.7242, "lng": 18.9658},
     # KWAZULU-NATAL
+    {"name": "Port Shepstone Hospital TCC", "lat": -30.7419, "lng": 30.4411},
+    {"name": "Stanger Hospital TCC (KwaDukuza)", "lat": -29.3364, "lng": 31.2956},
+    {"name": "Newcastle General TCC", "lat": -27.7558, "lng": 29.9314},
     {"name": "Addington TCC (Durban)", "lat": -29.8608, "lng": 31.0423},
     {"name": "Edendale TCC (PMB)", "lat": -29.6456, "lng": 30.3344},
     {"name": "RK Khan TCC (Chatsworth)", "lat": -29.9167, "lng": 30.8833},
@@ -21,42 +30,145 @@ THUTHUZELA_CENTRES = [
     {"name": "Mthatha General TCC", "lat": -31.5889, "lng": 28.7844},
     # FREE STATE
     {"name": "Pelonomi TCC (Bloemfontein)", "lat": -29.1386, "lng": 26.2553},
-    {"name": "Sasolburg TCC", "lat": -26.8167, "lng": 27.8167},
-    # MPUMALANGA / LIMPOPO / NW
-    {"name": "Rob Ferreira TCC (Nelspruit)", "lat": -25.4753, "lng": 30.9694},
-    {"name": "Dilokong TCC (Burgersfort)", "lat": -24.6667, "lng": 30.3333},
-    {"name": "Rustenburg TCC", "lat": -25.6661, "lng": 27.2413}
-]
+    {"name": "Sasolburg TCC (Metsimaholo)", "lat": -26.8167, "lng": 27.8167},
+    {"name": "Bongani TCC (Welkom)", "lat": -27.9733, "lng": 26.7351},
+    {"name": "Manapo TCC (QwaQwa)", "lat": -28.5333, "lng": 28.8167},
+    # MPUMALANGA
+    {"name": "Rob Ferreira TCC (Mbombela)", "lat": -25.4753, "lng": 30.9694},
+    {"name": "Ermelo TCC (Gert Sibande)", "lat": -26.5167, "lng": 29.9833},
+    {"name": "Witbank TCC (eMalahleni)", "lat": -25.8728, "lng": 29.2314},
+    {"name": "Themba TCC (Kabokweni)", "lat": -25.3333, "lng": 31.1333},
 
+    # NORTH WEST 
+    {"name": "Rustenburg TCC (Job Shimankana Tabane)", "lat": -25.6661, "lng": 27.2413},
+    {"name": "Klerksdorp/Tshepong TCC", "lat": -26.8833, "lng": 26.6667},
+    {"name": "Mafikeng TCC", "lat": -25.8558, "lng": 25.6401},
+    {"name": "Potchefstroom TCC", "lat": -26.7145, "lng": 27.1011},
+
+    # NORTHERN CAPE
+    {"name": "Kimberley TCC (Robert Sobukwe)", "lat": -28.7384, "lng": 24.7642},
+    {"name": "Upington TCC (Dr Harry Surtie)", "lat": -28.4552, "lng": 21.2536},
+    {"name": "De Aar TCC", "lat": -30.6481, "lng": 24.0114},
+    {"name": "Kuruman TCC", "lat": -27.4521, "lng": 23.4325},
+
+    #Limpopo
+    {"name": "Van Velden TCC (Tzaneen)", "lat": -23.8365, "lng": 30.1585},
+    {"name": "Dilokong TCC (Burgersfort)", "lat": -24.6667, "lng": 30.3333},
+    {"name": "Pietersburg TCC (Polokwane)", "lat": -23.9136, "lng": 29.4678},
+    {"name": "Tshilidzini TCC (Thohoyandou)", "lat": -23.0167, "lng": 30.4667},
+    {"name": "Nkhensani TCC (Giyani)", "lat": -23.3085, "lng": 30.7025},
+    {"name": "St. Ritas TCC (Glen Cowie)", "lat": -24.7833, "lng": 29.9333},
+    ]
 
 POLICE_STATIONS = [
     # GAUTENG
+    {"name": "Midrand SAPS", "lat": -25.9984, "lng": 28.1264, "prov": "GP"},
+    {"name": "Ivory Park SAPS (Midrand)", "lat": -26.0125, "lng": 28.1872, "prov": "GP"},
+    {"name": "Tembisa SAPS", "lat": -25.9964, "lng": 28.2272, "prov": "GP"},
+    {"name": "Rabie Ridge SAPS", "lat": -25.9912, "lng": 28.1745, "prov": "GP"},
+    {"name": "Randburg SAPS", "lat": -26.0925, "lng": 27.9947, "prov": "GP"},
+    {"name": "Alexandra SAPS", "lat": -26.1042, "lng": 28.1022, "prov": "GP"},
+    {"name": "Kempton Park SAPS", "lat": -26.1041, "lng": 28.2324, "prov": "GP"},
+    {"name": "Vanderbijlpark SAPS", "lat": -26.7025, "lng": 27.8341, "prov": "GP"},
+    {"name": "Vereeniging SAPS", "lat": -26.6738, "lng": 27.9261, "prov": "GP"},
+    {"name": "Springs SAPS", "lat": -26.2508, "lng": 28.4319, "prov": "GP"},
     {"name": "Johannesburg Central", "lat": -26.2067, "lng": 28.0311, "prov": "GP"},
     {"name": "Soweto (Diepkloof)", "lat": -26.2427, "lng": 27.9351, "prov": "GP"},
     {"name": "Pretoria Central", "lat": -25.7485, "lng": 28.1847, "prov": "GP"},
     {"name": "Sunnyside SAPS", "lat": -25.7511, "lng": 28.2136, "prov": "GP"},
     {"name": "Sandton", "lat": -26.1075, "lng": 28.0567, "prov": "GP"},
-    
+    {"name": "Tembisa South SAPS", "lat": -26.0355, "lng": 28.2142, "prov": "GP"},
+    {"name": "Rabie Ridge SAPS", "lat": -25.9912, "lng": 28.1745, "prov": "GP"},
+    {"name": "Oliefantsfontein SAPS", "lat": -25.9455, "lng": 28.2325, "prov": "GP"},
+    {"name": "Boksburg Central", "lat": -26.2167, "lng": 28.2500, "prov": "GP"},
+    {"name": "Benoni SAPS", "lat": -26.1883, "lng": 28.3153, "prov": "GP"},
+    {"name": "Brakpan SAPS", "lat": -26.2333, "lng": 28.3667, "prov": "GP"},
+    {"name": "Nigel SAPS", "lat": -26.4303, "lng": 28.4731, "prov": "GP"},
+    {"name": "Heidelberg SAPS", "lat": -26.5047, "lng": 28.3619, "prov": "GP"},
+
     # WESTERN CAPE
+    {"name": "Nyanga SAPS", "lat": -33.9961, "lng": 18.5914, "prov": "WC"},
+    {"name": "Gugulethu SAPS", "lat": -33.9781, "lng": 18.5714, "prov": "WC"},
+    {"name": "Mitchells Plain SAPS", "lat": -34.0484, "lng": 18.6056, "prov": "WC"},
+    {"name": "Paarl SAPS", "lat": -33.7242, "lng": 18.9658, "prov": "WC"},
     {"name": "Cape Town Central", "lat": -33.9249, "lng": 18.4241, "prov": "WC"},
     {"name": "Khayelitsha", "lat": -34.0381, "lng": 18.6751, "prov": "WC"},
     {"name": "Stellenbosch", "lat": -33.9321, "lng": 18.8602, "prov": "WC"},
     {"name": "George", "lat": -33.9587, "lng": 22.4597, "prov": "WC"},
-
+    {"name": "Philippi East SAPS", "lat": -34.0025, "lng": 18.5755, "prov": "WC"},
+    {"name": "Manenberg SAPS", "lat": -33.9842, "lng": 18.5472, "prov": "WC"},
+    {"name": "Delft SAPS", "lat": -33.9781, "lng": 18.6414, "prov": "WC"},
+    {"name": "Muizenberg SAPS", "lat": -34.1084, "lng": 18.4719, "prov": "WC"},
+    {"name": "Simon's Town SAPS", "lat": -34.1933, "lng": 18.4325, "prov": "WC"},
+    {"name": "Atlantis SAPS", "lat": -33.5667, "lng": 18.4833, "prov": "WC"},
+    
     # KWAZULU-NATAL
     {"name": "Durban Central", "lat": -29.8587, "lng": 31.0218, "prov": "KZN"},
     {"name": "Pietermaritzburg", "lat": -29.6006, "lng": 30.3794, "prov": "KZN"},
     {"name": "Umlazi", "lat": -29.9705, "lng": 30.8805, "prov": "KZN"},
+    {"name": "Inanda SAPS", "lat": -29.6975, "lng": 30.9325, "prov": "KZN"},        
+    {"name": "Phoenix SAPS", "lat": -29.7042, "lng": 31.0022, "prov": "KZN"},
+    {"name": "Pinetown SAPS", "lat": -29.8058, "lng": 30.8525, "prov": "KZN"},    
+    {"name": "Chatsworth SAPS", "lat": -29.9167, "lng": 30.8833, "prov": "KZN"},
+    {"name": "KwaMashu SAPS", "lat": -29.7542, "lng": 30.9822, "prov": "KZN"},
+    {"name": "Verulam SAPS", "lat": -29.6411, "lng": 31.0525, "prov": "KZN"},
+    {"name": "Empangeni SAPS", "lat": -28.7667, "lng": 31.8333, "prov": "KZN"},
+    {"name": "Richards Bay SAPS", "lat": -28.7833, "lng": 32.0333, "prov": "KZN"},
 
     # EASTERN CAPE
     {"name": "Gqeberha (Mount Road)", "lat": -33.9482, "lng": 25.5947, "prov": "EC"},
     {"name": "East London", "lat": -33.0153, "lng": 27.9015, "prov": "EC"},
     {"name": "Mthatha", "lat": -31.5889, "lng": 28.7844, "prov": "EC"},
+    {"name": "Mdantsane SAPS", "lat": -32.9333, "lng": 27.7167, "prov": "EC"},
+    {"name": "Zwide SAPS", "lat": -33.8825, "lng": 25.5614, "prov": "EC"},
+    {"name": "Humewood SAPS", "lat": -33.9781, "lng": 25.6414, "prov": "EC"},
 
-    # FREE STATE / LIMPOPO / MPUMALANGA / NW / NC
+    # FREE STATE  
     {"name": "Bloemfontein Central", "lat": -29.1181, "lng": 26.2231, "prov": "FS"},
-    {"name": "Polokwane", "lat": -23.9045, "lng": 29.4688, "prov": "LP"},
-    {"name": "Nelspruit", "lat": -25.4753, "lng": 30.9694, "prov": "MP"},
-    {"name": "Rustenburg", "lat": -25.6661, "lng": 27.2413, "prov": "NW"},
-    {"name": "Kimberley", "lat": -28.7282, "lng": 24.7499, "prov": "NC"}
-]
+    {"name": "Welkom SAPS", "lat": -27.9733, "lng": 26.7351, "prov": "FS"},
+    {"name": "Sasolburg SAPS", "lat": -26.8167, "lng": 27.8167, "prov": "FS"},
+    {"name": "Bethlehem SAPS", "lat": -28.2333, "lng": 28.3000, "prov": "FS"},
+    {"name": "Kroonstad SAPS", "lat": -27.6500, "lng": 27.2333, "prov": "FS"},
+    {"name": "Harrismith SAPS", "lat": -28.2667, "lng": 29.1333, "prov": "FS"},
+    {"name": "Phuthaditjhaba SAPS", "lat": -28.5333, "lng": 28.8167, "prov": "FS"},
+    
+    # MPUMALANGA 
+    {"name": "Nelspruit SAPS (Mbombela)", "lat": -25.4753, "lng": 30.9694, "prov": "MP"},
+    {"name": "Witbank SAPS (eMalahleni)", "lat": -25.8728, "lng": 29.2314, "prov": "MP"},
+    {"name": "Secunda SAPS", "lat": -26.5167, "lng": 29.1833, "prov": "MP"},
+    {"name": "Middelburg SAPS", "lat": -25.7725, "lng": 29.4611, "prov": "MP"},
+    {"name": "Ermelo SAPS", "lat": -26.5167, "lng": 29.9833, "prov": "MP"},
+    {"name": "White River SAPS", "lat": -25.3333, "lng": 31.0167, "prov": "MP"},
+    {"name": "Hazyview SAPS", "lat": -25.0435, "lng": 31.1255, "prov": "MP"},
+    {"name": "Piet Retief SAPS", "lat": -27.0000, "lng": 30.8000, "prov": "MP"},
+    
+    # NW 
+    {"name": "Rustenburg SAPS", "lat": -25.6661, "lng": 27.2413, "prov": "NW"},
+    {"name": "Potchefstroom SAPS", "lat": -26.7145, "lng": 27.1011, "prov": "NW"},
+    {"name": "Klerksdorp SAPS", "lat": -26.8667, "lng": 26.6667, "prov": "NW"},
+    {"name": "Mahikeng SAPS", "lat": -25.8558, "lng": 25.6401, "prov": "NW"},
+    {"name": "Brits SAPS", "lat": -25.6333, "lng": 27.7667, "prov": "NW"},
+    {"name": "Vryburg SAPS", "lat": -26.9500, "lng": 24.7333, "prov": "NW"},
+    {"name": "Lichtenburg SAPS", "lat": -26.1500, "lng": 26.1667, "prov": "NW"},
+    # NC
+    {"name": "Kimberley SAPS", "lat": -28.7282, "lng": 24.7499, "prov": "NC"},
+    {"name": "Upington SAPS", "lat": -28.4552, "lng": 21.2536, "prov": "NC"},
+    {"name": "De Aar SAPS", "lat": -30.6481, "lng": 24.0114, "prov": "NC"},
+    {"name": "Kuruman SAPS", "lat": -27.4521, "lng": 23.4325, "prov": "NC"},
+    {"name": "Springbok SAPS", "lat": -29.6643, "lng": 17.8865, "prov": "NC"},
+    {"name": "Kathu SAPS", "lat": -27.6975, "lng": 23.0488, "prov": "NC"},
+
+    #Limpopo
+    {"name": "Polokwane Central", "lat": -23.9045, "lng": 29.4688, "prov": "LP"},
+    {"name": "Tzaneen SAPS", "lat": -23.8332, "lng": 30.1635, "prov": "LP"},
+    {"name": "Thohoyandou SAPS", "lat": -23.0000, "lng": 30.4667, "prov": "LP"},
+    {"name": "Giyani SAPS", "lat": -23.3167, "lng": 30.7167, "prov": "LP"},
+    {"name": "Malamulele SAPS", "lat": -23.0167, "lng": 30.6833, "prov": "LP"},
+    {"name": "Modjadjiskloof SAPS", "lat": -23.7000, "lng": 30.1333, "prov": "LP"},
+    {"name": "Ritavi SAPS (Nkowankowa)", "lat": -23.8881, "lng": 30.2784, "prov": "LP"},
+    {"name": "Maake SAPS (Lenyenye)", "lat": -23.9355, "lng": 30.2055, "prov": "LP"},
+    {"name": "Phalaborwa SAPS", "lat": -23.9445, "lng": 31.1411, "prov": "LP"},
+    {"name": "Musina SAPS", "lat": -22.3333, "lng": 30.0333, "prov": "LP"},
+    {"name": "Bela-Bela SAPS", "lat": -24.8833, "lng": 28.2833, "prov": "LP"},
+    {"name": "Lephalale SAPS", "lat": -23.6667, "lng": 27.7500, "prov": "LP"}
+    ]
